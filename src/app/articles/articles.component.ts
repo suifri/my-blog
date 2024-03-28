5e88de51c9ed19c02405285484004cecf355c7b8
suifri <illia.khveshchuk.pz.2021@lpnu.ua>
2024-03-28 03:10:46 +0200

    add blog reading feature

---
+++ src/app/articles/articles.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable, map } from 'rxjs';
+
+@Component({
+  selector: 'app-articles',
+  templateUrl: './articles.component.html',
+  styleUrls: ['./articles.component.scss']
+})
+export class ArticlesComponent implements OnInit{
+  constructor(private scullyService: ScullyRoutesService){}
+
+  posts$: Observable<ScullyRoute[]> | undefined;
+
+  ngOnInit(): void {
+      this.posts$ = this.scullyService.available$.pipe(map(posts => posts.filter(post => post.title)));
+  }
+}
